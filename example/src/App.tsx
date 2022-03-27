dd42932507987aafff89b1062421b4bf9c06f6ca
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-03-27 09:39:08 -0400

    expand example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,14 +9,31 @@ const mathboxOptions = {
     klass: OrbitControls,
   },
 }
+const initialCameraPosition = [1, 1, 2]
+
+const expr = (emit: any, x: number, y: number, i: number, j: number, time: number) => {
+  emit(x, 0.25 + 0.25 * (Math.sin(x + time) * Math.sin(y + time)), y);
+};
+
+const Points = () => {
+  return (
+    <>
+      <MB.Area id="sampler" width={32} height={32} axes="xz" expr={expr} />
+      <MB.Point points="#sampler" color={0x3090ff} size={20}></MB.Point>
+      <MB.Transform position={[0, 0.5, 0]}>
+        <MB.Point points="#sampler" shape='diamond' color="green" size={20}></MB.Point>
+      </MB.Transform>
+    </>
+  )
+}
 
 function App() {
   const ref = useRef<any>();
   return (
-    <MB.Mathbox ref={ref} options={mathboxOptions} style={{ height: 500 }} initialCameraPosition={[0, 0, 3]}>
+    <MB.Mathbox ref={ref} options={mathboxOptions} className='h-100' initialCameraPosition={initialCameraPosition}>
       <MB.Cartesian >
-        <MB.Grid axes="xy" />
         <MB.Grid axes="xz" />
+        <Points />
       </MB.Cartesian>
     </MB.Mathbox>
   );

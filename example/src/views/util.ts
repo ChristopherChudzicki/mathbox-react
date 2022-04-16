7e10e17a9611cfdf264d6764d08251bedadc360d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-04-16 09:59:39 -0400

    reorganize examples (react-hook-form) (#26)
    This is in preparation to add more examples. The current example is probably too complicated...

---
+++ example/src/views/util.ts
@@ -0,0 +1,36 @@
+import { AreaEmitter } from "mathbox"
+import * as math from "mathjs"
+import memoizeOne from "memoize-one"
+
+export const isEvaluateableFunc = (argSymbols: string[], dimsOut: number) => {
+  if (dimsOut !== 1) throw new Error("Not implemented.")
+
+  return (text: string) => {
+    try {
+      const funcText = `f(${argSymbols}) = ${text}`
+      console.log(funcText)
+      const f = math.evaluate(funcText)
+      const args = argSymbols.map(() => 0)
+      const out = f(...args)
+      return typeof out === "number"
+    } catch (err) {
+      return false
+    }
+  }
+}
+
+const compile = memoizeOne((text: string): math.EvalFunction => {
+  return math.compile(text)
+})
+
+export const textToAreaEmitter = (
+  text: string,
+  scope: Record<string, number>
+): AreaEmitter => {
+  const funcText = `f(x,y,t) = ${text}`
+  const compiled = compile(funcText)
+  const f = compiled.evaluate(scope)
+  return (emit, x, y, i, j, t) => {
+    emit(x, f(x, y, t), y)
+  }
+}

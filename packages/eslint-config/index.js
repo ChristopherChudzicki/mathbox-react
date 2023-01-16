39a76bdf04f4b00e127393b86a463cc3cb2e6e6e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-01-16 14:38:34 -0500

    turbo

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,84 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.stories.*",
+          "**/*.spec.*",
+          "**/rollup.*",
+          "**/vite.*",
+        ],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "max-classes-per-file": "off",
+    "react/destructuring-assignment": "off",
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        filter: {
+          regex: "^_",
+          match: false,
+        },
+      },
+    ],
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        assert: "either"
+      }
+    ]
+  },
+  settings: {},
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {
+        "react/prop-types": "off",
+        "react/require-default-props": "off",
+        "react/jsx-props-no-spreading": "off",
+      },
+    },
+    {
+      files: ["**/*.stories.*"],
+      rules: {
+        "import/no-anonymous-default-export": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.*.json"]
+  },
+  ignorePatterns: ["build/", "node_modules/"]
+}

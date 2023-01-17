36b3130ae49806e0512d05121ff1c299200ba681
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-01-16 20:33:09 -0500

    lint with prettier

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -12,6 +12,7 @@ module.exports = {
   ],
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
+    "prettier/prettier": "error",
     "import/no-extraneous-dependencies": [
       "error",
       {
